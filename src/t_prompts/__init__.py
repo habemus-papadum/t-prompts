263d29dbb9858d3d300c36f532a870446310a46f
Nehal Patel <nehal@alum.mit.edu>
2025-10-14 09:27:00 -0400

    0.6.0

--- src/t_prompts/__init__.py
+++ src/t_prompts/__init__.py
@@ -24,7 +24,7 @@ from .exceptions import (
     UnsupportedValueTypeError,
 )
 
-__version__ = "0.6.0-alpha"
+__version__ = "0.6.0"
 __all__ = [
     "StructuredPrompt",
     "StructuredInterpolation",
