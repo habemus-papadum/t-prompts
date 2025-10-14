4f633b4741028028484918520e47f4e51e9cdd20
Nehal Patel <nehal@alum.mit.edu>
2025-10-14 07:55:35 -0400

    Bump to 0.5.0-alpha

--- src/t_prompts/__init__.py
+++ src/t_prompts/__init__.py
@@ -23,7 +23,7 @@ from .exceptions import (
     UnsupportedValueTypeError,
 )
 
-__version__ = "0.4.0"
+__version__ = "0.5.0-alpha"
 __all__ = [
     "StructuredPrompt",
     "StructuredInterpolation",
