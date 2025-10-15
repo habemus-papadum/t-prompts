7c4385e2432989ea673bd8adbe81c04bc122e4e4
Nehal Patel <nehal@alum.mit.edu>
2025-10-14 20:19:07 -0400

    Bump to 0.10.0-alpha

--- src/t_prompts/__init__.py
+++ src/t_prompts/__init__.py
@@ -32,7 +32,7 @@ from .parsing import (
 )
 from .text import process_dedent
 
-__version__ = "0.9.0"
+__version__ = "0.10.0-alpha"
 __all__ = [
     "StructuredPrompt",
     "StructuredInterpolation",
